ec7943180d28eff77b96e52849133fbf62a18235
webpandaaa <ankushrajsingh2004@gmail.com>
2024-06-04 00:21:31 +0530

    notfound page work successfully

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <>
-      NotFound
-    </>
+    <section className='page notfound'>
+      <div className='content'>
+        <img src="/notfound.png" alt="notfound" />
+        <Link to={"/"}> Return to Home </Link>
+      </div>
+      
+    </section>
   )
 }
 
